17f5519d48e7d8de2edfde432f6ab7cff8454d67
Phuc Than <quangphucthan@gmail.com>
2025-03-13 00:17:22 -0300

    [Home.jsx] Added Home page component

---
+++ my-portfolio/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <div>
+        <Header />
+        <Container>
+            <h1 className="display-1 text-center my-5">Welcome to My Portfolio</h1>
+            <p className="lead text-center mb-5">This is a simple portfolio website created using React.</p>
+        </Container>
+        <Footer />
+        </div>
+    );
+}
