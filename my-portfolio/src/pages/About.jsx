c6a6c9070063051673cc1648c0fe4df632ac789b
Phuc Than <quangphucthan@gmail.com>
2025-03-13 00:20:21 -0300

    [About.jsx] Create component

---
+++ my-portfolio/src/pages/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+    return (
+        <div>
+        <Header />
+        <Container>
+            <h1 className="display-1 text-center my-5">About Me</h1>
+            <p className="lead text-center mb-5">I am a web developer with a passion for creating beautiful and functional websites.</p>
+            <p className="text-center">I have experience with HTML, CSS, JavaScript, and React. I am currently learning Node.js and Express.</p>
+        </Container>
+        <Footer />
+        </div>
+    );
+}
