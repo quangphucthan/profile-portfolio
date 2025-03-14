0368f9dafe981b424d80f6fd1ad1d3cd7eb0a206
Phuc Than <quangphucthan@gmail.com>
2025-03-13 23:33:10 -0300

    [Update - App.jsx] Importing Header and Footer component, make changes to styling

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -8,17 +8,17 @@ import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
-    <div>
+    <div className="d-flex flex-column min-vh-100">
       <Header />
-      <main>
+      <main className="flex-grow-1">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      <Footer />
       </main>
+      <Footer />
     </div>
   );
 }
