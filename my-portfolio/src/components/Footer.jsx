1f42504567a9e9d19535b0a5f900d006b5c18df2
Phuc Than <quangphucthan@gmail.com>
2025-03-13 23:33:57 -0300

    [Update - Footer.jsx] Change text

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-dark text-light py-3 mt-5">
       <Container>
-        <p className="text-center mb-0">&copy; 2023 My Portfolio. All rights reserved.</p>
+        <p className="text-center mb-0">&copy; 2025 Phuc Than. All rights reserved.</p>
       </Container>
     </footer>
   );
