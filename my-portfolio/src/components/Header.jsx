ec836383da6fb5497d3487c0b955fca2666e6252
Phuc Than <quangphucthan@gmail.com>
2025-03-13 23:34:39 -0300

    [Update - Header.jsx] Fixed bugs

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,23 +1,20 @@
-import { NavBar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <NavBar bg="light" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <NavBar.Brand as={Link} to="/">My Portfolio</NavBar.Brand>
-        <NavBar.Toggle aria-controls="basic-navbar-nav" />
-
-        <NavBar.Collapse id="basic-navbar-nav">
+        <Navbar.Brand as={Link} to="/">My Portfolio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/about">About</Nav.Link>
             <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
           </Nav>
-        </NavBar.Collapse>
-
+        </Navbar.Collapse>
       </Container>
-    </NavBar>
+    </Navbar>
   );
 }
