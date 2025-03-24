346fdd3679a3f93d06139f00f640e23d09a1073c
Phuc Than <quangphucthan@gmail.com>
2025-03-23 23:00:30 -0300

    Implement searching for skills function

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,13 @@
 import { Container } from 'react-bootstrap';
+import Skills from '../components/Skills';
 
 export default function About() {
     return (
         <Container>
             <h1 className="display-1 text-center my-5">About Me</h1>
             <p className="lead text-center mb-5">I am a web developer with a passion for creating beautiful and functional websites.</p>
-            <p className="text-center">I have experience with HTML, CSS, JavaScript, and React. I am currently learning Node.js and Express.</p>
+            
+            <Skills />
         </Container>
     );
 }
